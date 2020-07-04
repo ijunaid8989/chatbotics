e26abac4b42cfc433c4e377121b9a9426306efa9
Junaid Farooq <junaid@evercam.io>
2020-07-04 18:44:45 +0500

    Handled starter payloads.

--- lib/mindvalley_web/controllers/webhook_controller.ex
+++ lib/mindvalley_web/controllers/webhook_controller.ex
@@ -1,5 +1,6 @@
 defmodule BuyItWeb.WebhookController do
   use BuyItWeb, :controller
+  import BuyIt.Utils
   require Logger
 
   @verify_token Application.get_env(:mindvalley, :webhook_token)
@@ -13,5 +14,45 @@ defmodule BuyItWeb.WebhookController do
     else
       _ -> send_resp(conn, :forbidden, "")
     end
+  end
+
+  def create(conn, %{
+    "entry" => [
+      %{
+        "messaging" => [
+          %{
+            "postback" => %{
+              "payload" => "GET_STARTED_PAYLOAD",
+            },
+            "sender" => %{"id" => psid},
+          }
+        ]
+      }
+    ],
+    "object" => "page"
+  }) do
+    quick_reply_to_payload(psid)
+    send_resp(conn, :ok, "")
+  end
+  def create(conn, %{
+    "entry" => [
+      %{
+        "messaging" => [
+          %{
+            "message" => %{
+              "quick_reply" => %{"payload" => payload_of_quick_reply},
+            },
+            "sender" => %{"id" => psid},
+          }
+        ]
+      }
+    ],
+    "object" => "page"
+  }) do
+    handle_quick_payload(payload_of_quick_reply, psid)
+    send_resp(conn, :ok, "")
+  end
+  def create(conn, params) do
+    send_resp(conn, :ok, "")
   end
 end
