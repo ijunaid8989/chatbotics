288893cb83f010c0efdb230f1cd315cb1e58af42
Junaid Farooq <junaid@evercam.io>
2020-07-05 22:01:28 +0500

    Used DETS to save flow.

--- lib/mindvalley/helpers/steps.ex
+++ lib/mindvalley/helpers/steps.ex
@@ -1,8 +1,10 @@
 defmodule BuyIt.Steps do
-  def save(psid, step), do: :dets.insert(:chat_flow, {psid, step})
+  def save(psid, step), do: :dets.insert(:chat_flow, {psid, step, []})
+  def save(psid, step, "SEARCH_BY_NAME" = payload), do: :dets.insert(:chat_flow, {psid, step, [payload]})
+  def save(psid, step, "SEARCH_BY_ID" = payload), do: :dets.insert(:chat_flow, {psid, step, [payload]})
 
   def get(psid) do
-    [{_psid, step}] = :dets.lookup(:chat_flow, psid)
-    step
+    [{_psid, step, payload}] = :dets.lookup(:chat_flow, psid)
+    {step, payload}
   end
 end
